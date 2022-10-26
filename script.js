279c15100e19719be004c1073ae0f19ecb9439d6
Juan Schallmoser <schallmoserjuan@gmail.com>
2022-10-26 11:59:05 -0300

    Concluido

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-( () =>{
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
 
- const btn = document.querySelector("[data-form-btn]");
+
+const btn = document.querySelector("[data-form-btn]");
 
 const createTask = (evento) => {
   evento.preventDefault();
@@ -20,35 +22,11 @@ const createTask = (evento) => {
   taskContent.appendChild(checkComplete());
   taskContent.appendChild(titleTask);
 
-  const content = `
-  ${checkComplete()} 
-    <i class="fas fa-trash-alt trashIcon icon"></i>`;
   //task.innerHTML = content;
   task.appendChild(taskContent);
+  task.appendChild(deleteIcon());
   list.appendChild(task);
-
-  console.log(content);
 };
-
-console.log(btn);
 
 //Arrow functions o funciones anonimas
 btn.addEventListener("click", createTask);
-
-const checkComplete = () => {
-  const i = document.createElement("i");
-  i.classList.add("far", "fa-check-square", "icon");
-  i.addEventListener("click", completeTask);
-  return i;
-};
-
-//IIFE
-//Immediately 
-const completeTask = (event) => {
-  const element = event.target;
-  element.classList.toggle("fas");
-  element.classList.toggle("completeIcon");
-  element.classList.toggle("far");
-};
-
-})();
